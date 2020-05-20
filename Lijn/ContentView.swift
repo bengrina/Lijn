f43c86ad79a8fd6ab52fd864002c36a18f7d1d65
Aymane Bengrina <bengrina.aymane@gmail.com>
2020-05-20 19:35:33 +0200

    Initial Commit

---
+++ Lijn/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Lijn
+//
+//  Created by Aymane on 20/05/2020.
+//  Copyright © 2020 Aymane Bengrina. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
