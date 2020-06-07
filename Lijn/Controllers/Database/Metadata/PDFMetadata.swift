b1301b5b7241044e42e9c470ab879851d107f121
Aymane Bengrina <bengrina.aymane@gmail.com>
2020-06-07 23:04:46 +0200

    add a function to generate thumbnails from PDF files

--- Lijn/Controllers/Database/Metadata/PDFMetadata.swift
+++ Lijn/Controllers/Database/Metadata/PDFMetadata.swift
@@ -10,8 +10,21 @@ import Foundation
 import PDFKit
 
 struct PDFMetadata {
-//    func getPDFMetadata(forFile file: URL) -> [String:String]{
-//        let pdf = PDFDocument(url: file)
-//        PDFDocumentAttribute(rawValue: <#T##String#>)
-//    }
+    // https://stackoverflow.com/a/48929065/13642472
+    func generateThumbnail(url: URL) -> UIImage?{
+        guard let data = try? Data(contentsOf: url),
+        let page = PDFDocument(data: data)?.page(at: 0) else {
+          return nil
+        }
+        let pageSize = page.bounds(for: .mediaBox)
+        let pdfScale = CGFloat(K.thumbWidth) / pageSize.width
+
+        // Apply if you're displaying the thumbnail on screen
+        let scale = UIScreen.main.scale * pdfScale
+        let screenSize = CGSize(width: pageSize.width * scale,
+                                height: pageSize.height * scale)
+
+        return page.thumbnail(of: screenSize, for: .mediaBox)
+        
+    }
 }
