bc820fc130f16b50d5b0e87415c0231024b9ff41
Aymane Bengrina <bengrina.aymane@gmail.com>
2020-06-14 21:57:24 +0200

    Change animation for ComicView transition

--- Lijn/Views/Library/CustomViewController.swift
+++ Lijn/Views/Library/CustomViewController.swift
@@ -11,35 +11,36 @@ import SwiftUI
 import UIKit
 
 struct ViewControllerHolder {
-         weak var value: UIViewController?
-    }
+    weak var value: UIViewController?
+}
 
-    struct ViewControllerKey: EnvironmentKey {
+struct ViewControllerKey: EnvironmentKey {
     static var defaultValue: ViewControllerHolder { return
         ViewControllerHolder(value:
-        UIApplication.shared.windows.first?.rootViewController ) }
-    }
+            UIApplication.shared.windows.first?.rootViewController ) }
+}
 
-    extension EnvironmentValues {
-        var viewController: ViewControllerHolder {
-            get { return self[ViewControllerKey.self] }
-            set { self[ViewControllerKey.self] = newValue }
-      }
+extension EnvironmentValues {
+    var viewController: ViewControllerHolder {
+        get { return self[ViewControllerKey.self] }
+        set { self[ViewControllerKey.self] = newValue }
     }
+}
 
-    extension UIViewController {
-         func present<Content: View>(style: UIModalPresentationStyle =
-         .automatic, @ViewBuilder builder: () -> Content) {
-             // Must instantiate HostingController with some sort of view...
-             let toPresent = UIHostingController(rootView:
-              AnyView(EmptyView()))
-             toPresent.modalPresentationStyle = style
-             // ... but then we can reset rootView to include the environment
-             toPresent.rootView = AnyView(
-             builder()
-            .environment(\.viewController, ViewControllerHolder(value:
-             toPresent))
-    )
-    self.present(toPresent, animated: true, completion: nil)
+extension UIViewController {
+    func present<Content: View>(style: UIModalPresentationStyle =
+        .automatic, @ViewBuilder builder: () -> Content) {
+        // Must instantiate HostingController with some sort of view...
+        let toPresent = UIHostingController(rootView:
+            AnyView(EmptyView()))
+        toPresent.modalPresentationStyle = style
+        toPresent.modalTransitionStyle = .crossDissolve
+        // ... but then we can reset rootView to include the environment
+        toPresent.rootView = AnyView(
+            builder()
+                .environment(\.viewController, ViewControllerHolder(value:
+                    toPresent))
+        )
+        self.present(toPresent, animated: true, completion: nil)
     }
 }
